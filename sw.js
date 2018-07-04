560d757ae33dc072035f4c297b87cccfc4304931
Christine Reffo <christine.reffo@gmail.com>
2018-07-04 11:52:55 -0700

    Bug: Service Worker not fetching cached files

---
+++ sw.js
@@ -0,0 +1,52 @@
+var cacheName = 'restaurant-v1';
+var cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/data/restaurants.json',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/css/styles.css',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/favicon.ico',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+  'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.js',
+  'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.css',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+];
+
+self.addEventListener("install", function(event) {
+  console.log("[ServicerWorker] Installed")
+
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("[ServiceWorker] Caching cacheFiles");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    console.log("[ServicerWorker] Activated")
+  )
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
--- sw/app.js
+++ sw/app.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
 
-  navigator.serviceWorker.register('/sw.js').then(function(registration){
+  navigator.serviceWorker.register('sw/sw.js').then(function(registration){
     console.log("Service Worker Registered");
   }).catch(function(err){
     console.log("Service Worker Failed to Register", err);
