c7dfaa0b1ba7e44d57ee619cae0878c3fc6e3dce
Christine Reffo <christine.reffo@gmail.com>
2018-07-03 21:58:20 -0700

    Bug fixing in progess: service worker caching

--- sw/sw.js
+++ sw/sw.js
@@ -3,29 +3,51 @@ var cacheFiles = [
   '/',
   '/index.html',
   '/restaurant.html',
-  // './data/restaurants.json',
+  '/data/restaurants.json',
   '/js/dbhelper.js',
   '/js/main.js',
   '/js/restaurant_info.js',
   '/css/styles.css',
-  // './sw/app.js'
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/sw/app.js',
+  '/favicon.ico',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+  'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.js',
+  'https://api.mapbox.com/mapbox-gl-js/v0.46.0/mapbox-gl.css',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
 ];
 
-self.addEventListener("install", function(e) {
+self.addEventListener("install", function(event) {
   console.log("[ServicerWorker] Installed")
 
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache){
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
       console.log("[ServiceWorker] Caching cacheFiles");
       return cache.addAll(cacheFiles);
     })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    console.log("[ServicerWorker] Activated")
   )
-})
+});
 
-self.addEventListener("activate", function(e) {
-  console.log("[ServicerWorker] Activated")
-})
-
-self.addEventListener("fetch", function(e) {
-  console.log("[ServicerWorker] Fetching, e.request.url")
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
 });
